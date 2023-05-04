d2d88bee7b2a9308d0cc369551d64a3f9be8797f
Weiren1996 <67635299+Weiren1996@users.noreply.github.com>
2023-05-04 15:43:13 +0800

    Update 段落分类tfidf.py

--- paragraph_classification/段落分类tfidf.py
+++ paragraph_classification/段落分类tfidf.py
@@ -35,9 +35,6 @@ def tokenize_text(text):
     tokens = []
     for sent in nltk.sent_tokenize(text):
         for word in nltk.word_tokenize(sent):
-            # 这里原先将长度小于2的字符串删除了
-            # if len(word) < 2:
-            #     continue
             tokens.append(word)
     return tokens
 
@@ -231,11 +228,11 @@ def predict_on_file(model_path, label_path, target_excel, predict_results_path):
     bow_vectorizer, bow_features = tfidf_extractor(all_corpus, ngram_range=(1, 1))
     y_test, X_test = get_vectors(bow_vectorizer, train_tagged, doc_label)
     log = LogisticRegression(max_iter=1000,class_weight='balanced').fit(X_test, y_test)
-    joblib.dump(log,model_path)  #r'E:/文本挖掘/工作三-工艺抽取/段落分类/101段落标记分类/model/log_2.pkl'
+    joblib.dump(log,model_path) 
 
     # 利用本地向量进行预测
     import xlrd
-    new_data = xlrd.open_workbook(target_excel)  #r"E:/文本挖掘/工作三-工艺抽取/段落分类/101段落标记分类/test/elsevier.xlsx"
+    new_data = xlrd.open_workbook(target_excel)
     sht = new_data.sheet_by_index(1)
     rows = sht.nrows
     col_0 = sht.col_values(0)
@@ -297,164 +294,8 @@ def predict_on_file(model_path, label_path, target_excel, predict_results_path):
         sht.cell(col_i, 2, tag)  # 写入表格的只能是字符型数据
         sht.cell(col_i, 4, str(pre_prob[col_i-1]))
         col_i += 1
-    xls.save(predict_results_path) #r'E:/文本挖掘/工作三-工艺抽取/段落分类/101段落标记分类/model\Elsevier.xlsx')
+    xls.save(predict_results_path)
     # log_model = joblib.load(model_path)
 
-# predict_on_file(r"/data/home/scv4785/run/wwr/text_classification/code/tfidf_out/model/tfidf_log",r"/data/home/scv4785/run/wwr/text_classification/code/tfidf_out/label.csv",r"/data/home/scv4785/run/wwr/text_classification/code/tfidf_out/all_paragraphs_predicted.xlsx",r"/data/home/scv4785/run/wwr/text_classification/code/tfidf_out/tfidf_predict_results.xlsx")
+# predict_on_file(r"./model/tfidf_log",r"./label.csv",r"./all_paragraphs_predicted.xlsx",r"./tfidf_predict_results.xlsx")
 get_accuracy_f1(lable_path,10)
-
-
-# csv_data = pd.read_csv(r'E:/文本挖掘/工作三-工艺抽取/段落分类/101段落标记分类\label_corpus.csv')
-# df = pd.DataFrame(csv_data)
-# df_shape = df.shape
-# length = df_shape[0]
-# outcome={}
-# for i in range(length):
-#     example_i = df.iloc[i]
-#     if len(example_i) > 0:
-#         outcome.update({example_i[0]:example_i[1]})
-# paragraph_list = list(outcome.keys())
-# english_punctuations = [',', '.', ':', ';', '?', '(', ')', '[', ']', '&', '!', '*', '@', '#', '$', '%']
-# words_list=[]
-# outcome_1={}
-# all_corpus = list()
-# doc_label = dict() #用于记录每一个自然段对应的标签
-# for i in range(len(paragraph_list)):
-#     r='[’!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]+'
-#     paragraph_0=re.sub(r,'',paragraph_list[i].lower())
-#     words_list_0 = nltk.word_tokenize(paragraph_0)
-#     words_list.append(words_list_0)
-#     keys = " ".join(words_list[i])
-#     all_corpus.append(keys)
-#     outcome_1.update({keys:df.iloc[i][1]})
-#     doc_label.update({keys:df.iloc[i][2]})
-# # 可以去重
-# df = pd.DataFrame(pd.Series(outcome_1), columns=['y'])   #将字典转化为dataframe的形式,第一列会变成索引列
-# df = df.reset_index().rename(columns={'index':'x'})
-# train = df
-# train, test = train_test_split(df, test_size=0.3, random_state=10)
-#
-# train_tagged = train.apply(
-#     lambda r: TaggedDocument(words=tokenize_text(r['x']), tags=[r.y]), axis=1)
-# # test_tagged = test.apply(
-# #     lambda r: TaggedDocument(words=tokenize_text(r['x']), tags=[r.y]), axis=1)
-# cores = multiprocessing.cpu_count()
-#
-#
-# def tfidf_extractor(corpus, ngram_range=(1,1)):
-#
-#     vectorizer = TfidfVectorizer(min_df=1, norm="l2", smooth_idf=True, use_idf=True,  ngram_range=ngram_range)
-#     features = vectorizer.fit_transform(corpus)
-#
-#     return vectorizer,features
-#
-# bow_vectorizer,bow_features = tfidf_extractor(all_corpus, ngram_range=(1,1))
-#
-#
-# def get_vectors(bow_vectorizer, tagged_docs, doc_label):
-#     sents = tagged_docs.values
-#     outcome = list()
-#     # infer_vec = model.infer_vector(doc.words, steps=5)
-#     for doc in sents:
-#         doc_list = list()
-#         doc_text = " ".join(doc.words)
-#         doc_list.append(doc_text)
-#         model_infer = bow_vectorizer.transform(doc_list)
-#         # print("model_infer",model_infer)
-#
-#         model_infer = model_infer.toarray()[0]
-#         # print("array",model_infer.shape)
-#         # print(model_infer)
-#
-#         if "experiment" in doc_label[doc_text].lower() or "method" in doc_label[doc_text].lower():
-#             tag_vec = np.ones(1)
-#             model_i = np.concatenate((model_infer,tag_vec),axis=0)
-#         else:
-#             tag_vec = np.zeros(1)
-#             model_i = np.concatenate((model_infer,tag_vec),axis=0)
-#             print(model_i.shape)
-#         outcome.append((doc.tags[0], model_i))
-#     targets, regressors = zip(*outcome)
-#     # targets, regressors = zip(*[(doc.tags[0], model.infer_vector(doc.words, steps=20)) for doc in sents])
-#     return targets, regressors
-#
-#
-# # y_train, X_train = get_vectors(bow_vectorizer, train_tagged, doc_label)
-# y_test, X_test = get_vectors(bow_vectorizer, test_tagged, doc_label)
-# log = LogisticRegression(max_iter=1000).fit(X_test, y_test)
-# # joblib.dump(log,r'E:/文本挖掘/工作三-工艺抽取/段落分类/101段落标记分类/model/log_2.pkl')
-#
-# # 利用本地向量进行预测
-# import xlrd
-#
-# new_data = xlrd.open_workbook(r"E:/文本挖掘/工作三-工艺抽取/段落分类/101段落标记分类/test/elsevier.xlsx")
-# sht = new_data.sheet_by_index(0)
-# rows = sht.nrows
-# # df.iloc[i]
-# col_0 = sht.col_values(0)
-# col_1 = sht.col_values(1)
-# outcome = dict()
-# outcome_v = list()
-# doi_outcome = dict()
-# for i in range(rows):
-#     words_list = []
-#     example_i = sht.row_values(i)
-#     if len(example_i) > 0:
-#         outcome.update({example_i[1]:example_i[0]})
-#
-#         text = example_i[1]
-#         r='[’!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]+'
-#         paragraph_=re.sub(r,'',text.lower())
-#         words_list_0 = nltk.word_tokenize(paragraph_)
-#         keys = " ".join(words_list_0)
-#         doi_outcome.update({keys:example_i[2]})
-# paragraph_list = list(outcome.keys())
-#
-# english_punctuations = [',', '.', ':', ';', '?', '(', ')', '[', ']', '&', '!', '*', '@', '#', '$', '%']
-# words_list=list()
-# outcome_1={}
-# sents = list() #用于记录每一个自然段对应的标签
-# doc_label = dict()
-# for i in range(len(paragraph_list)):
-#     r='[’!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]+'
-#     paragraph_0=re.sub(r,'',paragraph_list[i].lower())
-#     words_list_0 = nltk.word_tokenize(paragraph_0)
-#     words_list.append(words_list_0)
-#     keys = " ".join(words_list[i])
-#     doc_label.update({keys:col_1[i]})
-#
-# import openpyxl
-# xls = openpyxl.Workbook()
-# sht = xls.create_sheet(index=0)
-# col_i = 1
-#
-# for words in words_list:
-#     doc_text = " ".join(words)
-#     # model_infer = new_model.infer_vector(words, steps=20)
-#
-#     doc_list = list()
-#
-#     doc_list.append(doc_text)
-#     model_infer = bow_vectorizer.transform(doc_list)
-#     model_infer = model_infer.toarray()[0]
-#
-#
-#     if "experiment" in doc_label[doc_text].lower() or "method" in doc_label[doc_text].lower():
-#         tag_vec = np.ones(1)
-#         model_i = np.concatenate((model_infer,tag_vec),axis=0)
-#     else:
-#         tag_vec = np.zeros(1)
-#         model_i = np.concatenate((model_infer,tag_vec),axis=0)
-#     outcome_v.append(model_i)
-#     sht.cell(col_i,1,doc_text)
-#     doi = doi_outcome[doc_text]
-#     sht.cell(col_i,3,doi)
-#     col_i += 1
-#
-# predict_outcome = log.predict(tuple(outcome_v))
-#
-# col_i = 1
-# for tag in predict_outcome:
-#     sht.cell(col_i,2,tag) # 写入表格的只能是字符型数据
-#     col_i += 1
-# xls.save(r'E:/文本挖掘/工作三-工艺抽取/段落分类/101段落标记分类/model\Elsevier.xlsx')
